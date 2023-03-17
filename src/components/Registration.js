9e8723cf3e83caf2bc9fdf09e431469cb92a035f
Ganesh <ch.tejamr@gmail.com>
2023-03-18 02:23:23 +0530

    edited few styles of navbar and few styles of gradient and headings of sign-up

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -2,9 +2,9 @@ import React from "react"
 import Gradient_Headtag from "./sub-components/Gradient_Headtag"
 
 export default function Registration() {
-    return (
-        <div className="registration">
-            <Gradient_Headtag />
-        </div>
-    )
+  return (
+    <div className="registration">
+      <Gradient_Headtag />
+    </div>
+  )
 }
